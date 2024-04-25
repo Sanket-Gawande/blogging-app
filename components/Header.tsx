a5d78406409eb1b6009f4c0265b3d42e05aafbae
Jay Pokale <jay.pokale.35@gmail.com>
2024-04-25 11:35:23 +0530

    Homepage

---
+++ components/Header.tsx
@@ -0,0 +1,37 @@
+const Header = () => {
+  return (
+    <header className="sticky top-0 backdrop-filter backdrop-blur-sm text-gray-300 mx-auto max-w-7xl py-2 h-12 z-10">
+      <div className="flex items-center justify-between px-4">
+        <div className="flex-shrink-0">
+          <h1 className="text-xl font-bold">Blog</h1>
+        </div>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="/" className="hover:text-gray-300">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/about" className="hover:text-gray-300">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="/services" className="hover:text-gray-300">
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="/contact" className="hover:text-gray-300">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
