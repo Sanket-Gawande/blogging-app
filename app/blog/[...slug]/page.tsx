9a03ba8c9d846a4035976e9ee08c7bd8ad06a2e0
Jay Pokale <jay.pokale.35@gmail.com>
2024-05-17 15:34:43 +0530

    Code blocks

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -2,6 +2,9 @@ import formatDate from "@/lib/formatDate";
 import Head from "next/head";
 import Image from "next/image";
 import Markdown from "react-markdown";
+import rangeParser from "parse-numeric-range";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 export const revalidate = 3600;
 
@@ -16,6 +19,48 @@ const PostPage = async ({ params }: { params: { slug: string[] } }) => {
   const [slug] = slugArr;
   const data = await getPost(slug);
   const { post } = await data.json();
+
+  const syntaxTheme = oneDark;
+
+  const MarkdownComponents: object = {
+    code({ node, inline, className, ...props }: any) {
+      const hasLang = /language-(\w+)/.exec(className || "");
+      const hasMeta = node?.data?.meta;
+
+      const applyHighlights: object = (applyHighlights: number) => {
+        if (hasMeta) {
+          const RE = /{([\d,-]+)}/;
+          const metadata = node.data.meta?.replace(/\s/g, "");
+          const strlineNumbers = RE?.test(metadata)
+            ? RE?.exec(metadata)![1]
+            : "0";
+          const highlightLines = rangeParser(strlineNumbers);
+          const highlight = highlightLines;
+          const data = highlight.includes(applyHighlights) ? "highlight" : null;
+          return { data };
+        } else {
+          return {};
+        }
+      };
+
+      return hasLang ? (
+        <SyntaxHighlighter
+          style={syntaxTheme}
+          language={hasLang[1]}
+          PreTag="div"
+          className="codeStyle"
+          showLineNumbers={true}
+          wrapLines={hasMeta}
+          useInlineStyles={true}
+          lineProps={applyHighlights}
+        >
+          {props.children}
+        </SyntaxHighlighter>
+      ) : (
+        <code className={className} {...props} />
+      );
+    },
+  };
 
   return (
     <>
@@ -51,7 +96,7 @@ const PostPage = async ({ params }: { params: { slug: string[] } }) => {
           id="__markdown_content__"
           className="w-11/12 mx-auto space-y-6 max-w-3xl leading-6 text-slate-11 py-4"
         >
-          <Markdown>{post.content}</Markdown>
+          <Markdown components={MarkdownComponents}>{post.content}</Markdown>
         </section>
       </section>
     </>
